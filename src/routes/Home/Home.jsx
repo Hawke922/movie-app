455176abc1f42a8bd3423744eeef1797d261d46a
Hawke922 <tom.klucar@gmail.com>
2023-04-02 10:59:07 +0200

    infinite scroll

--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -1,55 +1,139 @@
-import React, { useEffect, useState } from 'react'
-import { useQuery } from '@tanstack/react-query'
+import React, { useState, useCallback, useRef } from 'react'
+import classnames from 'classnames'
+import { useInfiniteQuery } from '@tanstack/react-query'
 
 import { useDebounce } from '/src/helpers/hooks.js'
+
+import classes from './Home.module.scss'
 
 const Home = () => {
   const [searchValue, setSearchValue] = useState('')
 
-  const apiKey = import.meta.env.VITE_OMDB_API_KEY
-
   const debouncedSearchValue = useDebounce(searchValue, 500)
 
-  const { isLoading, isError, isPaused, error, data } = useQuery({
+  const apiKey = import.meta.env.VITE_OMDB_API_KEY
+
+  const {
+    fetchNextPage,
+    hasNextPage,
+    isFetching,
+    isError,
+    error,
+    data,
+    status,
+  } = useInfiniteQuery({
     queryKey: ['movies', debouncedSearchValue],
-    queryFn: () => {
-      return fetch(
-        `http://www.omdbapi.com/?apikey=${apiKey}&s=${debouncedSearchValue}&type=movie`
-      ).then((res) => res.json())
+    queryFn: async ({ pageParam }) => {
+      const res = await fetch(
+        `http://www.omdbapi.com/?apikey=${apiKey}&s=${debouncedSearchValue}&type=movie&page=${pageParam}`
+      )
+
+      return await res.json()
     },
     enabled: debouncedSearchValue !== '',
+    getNextPageParam: (lastPage, pages) => {
+      if (lastPage.Response === 'False') {
+        return undefined
+      }
+
+      const lengthOfLoadedMovies = pages.reduce((acc, page) => {
+        return acc + page.Search.length
+      }, 0)
+
+      const areAllMoviesLoaded =
+        lengthOfLoadedMovies === Number(lastPage.totalResults)
+
+      return areAllMoviesLoaded ? undefined : pages.length + 1
+    },
   })
 
   const handleChange = (event) => {
     setSearchValue(event.target.value)
   }
 
-  console.log(isPaused)
+  const observer = useRef()
+
+  const lastMovieRef = useCallback(
+    (node) => {
+      if (isFetching) {
+        return
+      }
+
+      if (observer.current) {
+        observer.current.disconnect()
+      }
+
+      observer.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting && hasNextPage) {
+          fetchNextPage()
+        }
+      })
+
+      if (node) {
+        observer.current.observe(node)
+      }
+    },
+    [isFetching, hasNextPage]
+  )
 
   const renderMovies = () => {
-    if (isLoading) {
-      return <div>Loading...</div>
+    if (debouncedSearchValue === '') {
+      return <div>Search for a movie</div>
+    }
+
+    if (status === 'pending' || data === undefined) {
+      return <div className={classes['movie-list__loader']} />
     }
 
     if (isError) {
       return <div>Error: {error.message}</div>
     }
-    console.log(data)
+
+    // TODO: constants for response types
+    if (data.pages[0].Response === 'False') {
+      return <div>No movies found</div>
+    }
+
+    const movies = data.pages.reduce((acc, page) => {
+      return [...acc, ...page.Search]
+    }, [])
 
     return (
-      <ul>
-        {data.Search.map((movie) => (
-          <li key={movie.imdbID}>{movie.Title}</li>
-        ))}
+      <ul className={classes['movie-list']}>
+        {movies.map((movie, index) => {
+          if (movies.length === index + 1) {
+            return (
+              <li
+                key={movie.imdbID}
+                className={classes['movie-list__movie']}
+                ref={lastMovieRef}
+              >
+                {movie.Title}
+              </li>
+            )
+          }
+
+          return (
+            <li key={movie.imdbID} className={classes['movie-list__movie']}>
+              {movie.Title}
+            </li>
+          )
+        })}
+        <div
+          className={classnames(classes['movie-list__loader'], {
+            [classes['movie-list__loader--hidden']]: hasNextPage === false,
+          })}
+        />
       </ul>
     )
   }
 
   return (
-    <div>
+    <div className={classes.wrapper}>
       <input
+        className={classes.input}
         type='text'
-        placeholder='Search a movie'
+        placeholder='Search for a movie'
         value={searchValue}
         onChange={handleChange}
       />
